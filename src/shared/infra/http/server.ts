5c187852a29b1af452c1d813b827050739579f78
leolivm <leolivm@outlook.com>
2020-11-24 15:26:32 -0300

    fix reteLimiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -13,10 +13,10 @@ import "@shared/container";
 
 const app = express();
 
-app.use(rateLimiterMiddleware);
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(multerConfig.uploadsFolder));
+app.use(rateLimiterMiddleware);
 app.use(routes);
 
 app.use(errors());
